27d33a5abbace485ce1d28c5e4ad68a59b2b3c69
José Luiz <luizsobrinho3000@hotmail.com>
2019-01-31 10:02:54 -0300

    Parametrização da funcao loadView

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -33,12 +34,17 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+
+		});
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> { 
+		});
 	}
 
 	@Override
@@ -46,41 +52,25 @@ public class MainViewController implements Initializable {
 		// TODO Auto-generated method stub
 
 	}
+	// synchronized garante que a aplicação não de erro durante amultitread
+	//Função parametrizada com a função lambda
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) { 
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newvBox = loader.load();
+			Scene mainScene = Main.getMainScene();//
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
 
-	private synchronized void loadView(String absoluteName) { //synchronized garante que a aplicação não de erro durante a multitread
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newvBox = loader.load();
-			Scene mainScene = Main.getMainScene();//
-			VBox mainVbox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
-			
 			Node mainMenu = mainVbox.getChildren().get(0);
 			mainVbox.getChildren().clear();
 			mainVbox.getChildren().add(mainMenu);
 			mainVbox.getChildren().addAll(newvBox.getChildren());
+
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		} catch (IOException e) {
 			Alerts.showAlert("IOException", "Error Loading View", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
-	private synchronized void loadView2(String absoluteName) { //synchronized garante que a aplicação não de erro durante a multitread
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newvBox = loader.load();
-			Scene mainScene = Main.getMainScene();//
-			VBox mainVbox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
-			
-			Node mainMenu = mainVbox.getChildren().get(0);
-			mainVbox.getChildren().clear();
-			mainVbox.getChildren().add(mainMenu);
-			mainVbox.getChildren().addAll(newvBox.getChildren());
-			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
-			
-		} catch (IOException e) {
-			Alerts.showAlert("IOException", "Error Loading View", e.getMessage(), AlertType.ERROR);
-		}
-	}
+
 }
